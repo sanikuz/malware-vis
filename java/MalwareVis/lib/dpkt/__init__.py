e725c50789a167d17c81e3d723b3f3d96043c6d9
opmiss <opmiss@gmail.com>
2012-08-21 16:58:51 +0000

    add source code

---
+++ java/MalwareVis/lib/dpkt/__init__.py
@@ -0,0 +1,69 @@
+# $Id: __init__.py 54 2008-12-19 23:02:13Z jon.oberheide $
+
+"""fast, simple packet creation and parsing."""
+
+__author__ = 'Dug Song <dugsong@monkey.org>'
+__copyright__ = 'Copyright (c) 2004 Dug Song'
+__license__ = 'BSD'
+__url__ = 'http://monkey.org/~dugsong/dpkt/'
+__version__ = '1.6'
+
+from dpkt import *
+
+import ah
+import aim
+import arp
+import asn1
+import bgp
+import cdp
+import dhcp
+import diameter
+import dns
+import dtp
+import esp
+import ethernet
+import gre
+import gzip
+import h225
+import hsrp
+import http
+import icmp
+import icmp6
+import igmp
+import ip
+import ip6
+import ipx
+import loopback
+import mrt
+import netbios
+import netflow
+import ntp
+import ospf
+import pcap
+import pim
+import pmap
+import ppp
+import pppoe
+import qq
+import radius
+import rfb
+import rip
+import rpc
+import rtp
+import rx
+import sccp
+import sctp
+import sip
+import sll
+import smb
+import ssl
+import stp
+import stun
+import tcp
+import telnet
+import tftp
+import tns
+import tpkt
+import udp
+import vrrp
+import yahoo
